e7d1fc5f710df29743553da19b2b40e9689d4214
DeLemosCoding <souzaj553@gmail.com>
2026-09-13 22:06:56 -0300

    Criei o admin corretamente
    Criação do admin (super usuário) concluída com sucesso.

--- consulta/admin.py
+++ consulta/admin.py
@@ -1,3 +1,20 @@
 from django.contrib import admin
+from .models import Doctor, Pacient, Consultation
 
 # Register your models here.
+
+@admin.register(Doctor)
+class BrandAdmin(admin.ModelAdmin):
+    list_display = ('name', 'birth_date', 'expertise', 'CRM',)
+    search_fields = ('name', 'expertise',)
+
+@admin.register(Pacient)
+class BrandAdmin(admin.ModelAdmin):
+    list_display = ('name', 'birth_date', 'CPF', 'gender',)
+    search_fields = ('name',)
+
+@admin.register(Consultation)
+class BrandAdmin(admin.ModelAdmin):
+    list_display = ('doctor', 'pacient', 'date',)
+    list_filter = ('doctor', 'date',)
+    search_fields = ('doctor', 'date',)
